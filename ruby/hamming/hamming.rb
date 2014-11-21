49a9d93089a6973d7c07fcb55e2367de9c3ed732
Nathan Wallace <nathan.wallace@leandogsoftware.com>
2014-11-21 13:22:06 -0500

    ruby/hamming 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,17 +1,8 @@
 class Hamming
 
   def self.compute(a, b)
-    distance, i = 0, 0
-    until done?(a, b, i)
-      distance += 1 if a[i] != b[i]
-      i += 1
+    a.each_char.each_with_index.count do |a_char, i|
+      b[i] && a_char != b[i]
     end
-    distance
-  end
-
-  private
-
-  def self.done?(a, b, i)
-    a[i].nil? || b[i].nil?
   end
 end
