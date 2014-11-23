b8f9819b932fb08f416cd857f1a254c403c6473b
Nathan Wallace <nathan.m.wallace@gmail.com>
2014-11-23 12:59:14 -0500

    clojure/rna_transcription 2

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -2,6 +2,5 @@
 
 (defn to-rna [dna_strand]
   (def complements {"C" "G" "G" "C" "A" "U" "T" "A"})
-  (def rna_complements (str "[" (keys complements) "]"))
-  (assert (re-matches (re-pattern (str rna_complements "+")) dna_strand))
-  (clojure.string/replace dna_strand (re-pattern rna_complements) complements))
+  (assert (re-matches (re-pattern (str "[" (keys complements) "]+")) dna_strand))
+  (clojure.string/replace dna_strand #"." complements))
