500af6540826b38d4e20a8ec084c832147bd932a
Nathan Wallace <nathan.m.wallace@gmail.com>
2014-11-25 20:22:59 -0500

    clojure/word_count 2

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -1,10 +1,5 @@
 (ns word-count
-  (:require [clojure.string :refer [lower-case replace split]]))
+  (:require [clojure.string :refer [lower-case]]))
 
 (defn word-count [input-str]
-  (reduce (fn [counts word]
-            (if (contains? counts word)
-              (assoc counts word (inc (counts word)))
-              (assoc counts word 1)))
-          {}
-          (split (lower-case (replace input-str #"\W" " ")) #"\s+")))
+  (frequencies (re-seq #"\w+" (lower-case input-str))))
