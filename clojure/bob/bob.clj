6a509357a352fe3516fdbf4c6ddd030afa30b4f1
Nathan Wallace <nathan.m.wallace@gmail.com>
2014-11-30 18:23:37 -0500

    clojure/bob 3

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,17 +1,19 @@
 (ns bob)
 
-(defn silence? [statement]
-  (clojure.string/blank? statement))
-
-(defn shouting? [statement]
-  (and (re-matches #"[^a-z]+" statement) (re-find #"[A-Z]" statement)))
-
-(defn question? [statement]
-  (re-find #"\?$" statement))
-
 (defn response-for [statement]
-  (cond
-    (silence?  statement) "Fine. Be that way!"
-    (shouting? statement) "Whoa, chill out!"
-    (question? statement) "Sure."
-    :else                 "Whatever."))
+
+  (defn silence? [statement]
+    (clojure.string/blank? statement))
+
+  (defn shouting? [statement]
+    (and (re-matches #"[^a-z]+" statement) (re-find #"[A-Z]" statement)))
+
+  (defn question? [statement]
+    (re-find #"\?$" statement))
+
+  (def responses [{:check silence?  :response "Fine. Be that way!"}
+                  {:check shouting? :response "Whoa, chill out!"}
+                  {:check question? :response "Sure."}])
+
+  (:response (or (first (filter #((:check %) statement) responses))
+                 {:response "Whatever."})))
