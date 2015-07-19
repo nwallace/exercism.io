c7c783b38f5e98276bfc790142946ceb62257667
Nathan Wallace <nathan.m.wallace@gmail.com>
2015-07-18 22:13:07 -0400

    clojure/grade_school 3

--- clojure/grade-school/grade_school.clj
+++ clojure/grade-school/grade_school.clj
@@ -1,17 +1,24 @@
 (ns grade-school)
 
-(defn- map-values [mapping-func coll]
+(defn- update-values [coll mapping-func]
   (reduce-kv
-    (fn [memo k v]
-      (assoc memo k (mapping-func v)))
+    (fn [memo k v] (assoc memo k (mapping-func v)))
     {}
     coll))
+
+(defn- init-or-conj [list-or-nil item]
+  (conj (or list-or-nil []) item))
 
 (defn grade [db grade-number]
   (db grade-number []))
 
 (defn add [db student grade-number]
-  (assoc db grade-number (conj (grade db grade-number) student)))
+  (update-in db [grade-number] init-or-conj student))
 
 (defn sorted [db]
-  (into (sorted-map) (map-values sort db)))
+  (into (sorted-map) (update-values db sort)))
+
+(let [coll {:a [1 10 4] :b [7 1 2]}]
+  (update-in coll
+             [:b]
+             sort))
