498546914f7a4a6b95706dfbaadaf7bd099a5d92
Nathan Wallace <nathan@nosuchthingastwo.com>
2016-01-21 22:57:35 -0500

    clojure/etl 2

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,12 +1,13 @@
 (ns etl
   (:require [clojure.string :refer [lower-case]]))
 
-(defn- ungroup-and-transform-values [transformation accumulator [k values]]
-  (conj accumulator
-        (reduce (fn [result value]
-                  (conj result {(transformation value) k}))
-                {} values)))
+(defn- transformer-and-ungrouper [old-key transformation]
+  (fn [result value]
+    (assoc result (transformation value) old-key)))
 
+(defn- ungroup-and-transform-values [transformation accumulator [old-key values]]
+  (conj accumulator
+        (reduce (transformer-and-ungrouper old-key transformation) {} values)))
 
 (defn transform [data]
   (reduce (partial ungroup-and-transform-values lower-case) {} data))
