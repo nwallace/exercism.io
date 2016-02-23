a6c438970f8335fa84d9338823e87dcc2dead0ea
Nathan Wallace <nathan@nosuchthingastwo.com>
2016-02-22 21:34:46 -0500

    clojure/binary 2

--- clojure/binary/src/binary.clj
+++ clojure/binary/src/binary.clj
@@ -1,18 +1,14 @@
 (ns binary)
 
 (defn- binary? [string]
-  (re-find #"\A[01]*\Z" string))
+  (every? #{\0 \1} string))
 
-(defn- pair-with-powers [binary]
-  (map vector (reverse binary) (iterate inc 0)))
-
-(defn- bit-pair->val [[bit power]]
-  (* (Integer/parseInt (str bit)) (int (Math/pow 2 power))))
+(defn- bits->dec-values [binary]
+  (map #(* (Integer/parseInt (str %1)) %2)
+       (reverse binary)
+       (iterate (partial * 2) 1)))
 
 (defn to-decimal [binary]
   (if (binary? binary)
-    (->> binary
-         pair-with-powers
-         (map bit-pair->val)
-         (reduce +))
+    (reduce + (bits->dec-values binary))
     0))
