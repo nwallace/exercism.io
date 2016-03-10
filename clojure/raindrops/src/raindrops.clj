b6e6aa4924a0897f30969e6788a0582c17111c15
Nathan Wallace <nathan@nosuchthingastwo.com>
2016-03-09 21:04:00 -0500

    clojure/raindrops 2

--- clojure/raindrops/src/raindrops.clj
+++ clojure/raindrops/src/raindrops.clj
@@ -5,24 +5,12 @@
                       5 "Plang"
                       7 "Plong"})
 
-(defn- factorization [n]
-  (loop [n n
-         factor 2
-         factors []]
-    (cond
-      (< n 2) factors
-      (zero? (mod n factor)) (recur (/ n factor) factor (conj factors factor))
-      :else (recur n (inc factor) factors))))
-
-(defn- select-values [coll keys]
-  (map val (select-keys coll keys)))
-
-(defn- pl*ng-erize [n]
-  (let [unique-factors (apply sorted-set (factorization n))]
-    (apply str (select-values pl*ng unique-factors))))
-
 (defn convert [n]
-  (let [pl*ng-erization (pl*ng-erize n)]
-    (if (string/blank? pl*ng-erization)
-      (str n)
-      pl*ng-erization)))
+  (if-let
+    [pl*ngs (seq (map val
+                      (filter
+                        (comp zero? (partial mod n) first)
+                        pl*ng)))]
+    (apply str pl*ngs)
+    (str n)))
+
