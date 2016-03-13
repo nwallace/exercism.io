b92e8119444b1c3a18ad6a32b43d5882d6d2cfc6
Nathan Wallace <nathan@nosuchthingastwo.com>
2016-03-13 16:13:09 -0400

    clojure/allergies 2

--- clojure/allergies/src/allergies.clj
+++ clojure/allergies/src/allergies.clj
@@ -1,32 +1,19 @@
 (ns allergies)
 
 (def allergens
-  [:cats       128
-   :pollen      64
-   :chocolate   32
-   :tomatoes    16
-   :strawberries 8
-   :shellfish    4
-   :peanuts      2
-   :eggs         1])
-
-(defn- remove-unknown-scores [score]
-  (let [max-single-score (* 2 (second allergens))]
-    (loop [score score]
-      (if (>= score max-single-score)
-        (recur (- score max-single-score))
-        score))))
+  [:eggs
+   :peanuts
+   :shellfish
+   :strawberries
+   :tomatoes
+   :chocolate
+   :pollen
+   :cats])
 
 (defn allergies [score]
-  (loop [score (remove-unknown-scores score)
-         allergens allergens
-         result '()]
-    (let [allergen (first allergens)
-          allergen-score (second allergens)]
-      (cond
-        (or (< score 1) (empty? allergens)) result
-        (>= score allergen-score) (recur (- score allergen-score) (nnext allergens) (conj result allergen))
-        :else (recur score (nnext allergens) result)))))
+  (for [[allergen i] (zipmap allergens (range))
+        :when (bit-test score i)]
+    allergen))
 
 (defn allergic-to? [score allergen]
   (some #{allergen} (allergies score)))
