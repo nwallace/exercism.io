febfbb12f9a154a7c64a68226e982cdbde9bdd2a
Nathan Wallace <nathan@nosuchthingastwo.com>
2016-03-18 08:57:59 -0400

    clojure/queen-attack 1

---
+++ clojure/queen-attack/src/queen_attack.clj
@@ -0,0 +1,24 @@
+(ns queen-attack
+  (:require [clojure.string :refer [upper-case join]]))
+
+(def ^:private board-size 8)
+
+(defn board-string [queens]
+  (let [empty-board (->> (range board-size)
+                         (map (fn [row]
+                                (map (constantly "_") (range board-size))))
+                         (map vec) vec)
+        populated-board (reduce-kv (fn [board color coords]
+                                     (assoc-in board coords
+                                               (upper-case (name color))))
+                                   empty-board queens)]
+    (as-> populated-board $
+          (map (partial join " ") $)
+          (interleave $ (repeat "\n"))
+          (apply str $))))
+
+(defn can-attack [{:keys [w b]}]
+  (let [paired-coords (partition 2 (interleave w b))
+        straight-across? (some #(true? (apply = %)) paired-coords)
+        diagonal? (apply = (apply map - paired-coords))]
+    (or straight-across? diagonal?)))
